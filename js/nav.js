ac825d81b99a91ba4b534f6e6a303dc28edf862e
Daniel Sánchez <139398686+DanielSanchezCasas@users.noreply.github.com>
2023-07-13 23:14:49 -0500

    navigation animation improvement

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,52 @@
-function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%";
+function checkHeight(mobileMenu, targetHeight, applyStyle, removeStyle) {
+  var currentHeight = mobileMenu.getBoundingClientRect().height;
+  var overlayContent = document.querySelector(".overlay-content");
+
+  if (applyStyle(currentHeight, targetHeight) && overlayContent.style.borderBottom === "") {
+    overlayContent.style.borderBottom = "solid 7px #fffe7e"; // Reemplaza #fffe7e con el color deseado
+  }
+
+  if (removeStyle(currentHeight, targetHeight) && overlayContent.style.borderBottom !== "") {
+    overlayContent.style.borderBottom = ""; // Elimina el estilo de borde
+  }
+
+  if (currentHeight !== targetHeight) {
+    requestAnimationFrame(function () {
+      checkHeight(mobileMenu, targetHeight, applyStyle, removeStyle);
+    });
+  }
 }
 
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%";
+function openNav() {
+  var mobileMenu = document.getElementById("mobile-menu");
+  var targetHeight = 5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+  mobileMenu.style.height = "100%";
+
+  function applyStyle(currentHeight, targetHeight) {
+    return currentHeight >= targetHeight;
+  }
+
+  function removeStyle(currentHeight, targetHeight) {
+    return currentHeight <= targetHeight;
+  }
+
+  checkHeight(mobileMenu, targetHeight, applyStyle, removeStyle);
+}
+
+function closeNav() {
+  var mobileMenu = document.getElementById("mobile-menu");
+  var targetHeight = 5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+  function applyStyle(currentHeight, targetHeight) {
+    return currentHeight >= targetHeight;
+  }
+
+  function removeStyle(currentHeight, targetHeight) {
+    return currentHeight <= targetHeight;
+  }
+
+  checkHeight(mobileMenu, targetHeight, applyStyle, removeStyle);
+
+  mobileMenu.style.height = "0%";
 }
